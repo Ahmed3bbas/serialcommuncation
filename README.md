ba9eabe250378d57c0e7553ff66f291350801726
Ahmed <eng.ahmedkhaled53@gmail.com>
2019-12-18 09:00:15 +0200

    reading port & plotting read data

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Serial communication with python
 
-This progam has two main functions:
+This progam has four main functions:
 
 * get, connect, and disconnect with available ports  
 * send data to connected port
+* read 7 bytes (@ amp time3 time2 time1 time0 ;) from connected port 
+* plotting reading data 
 
 ## Getting Started
 
@@ -19,6 +21,7 @@ python3 main.py
 * qt5
 * pyserial
 * glob
+* matplotlib
 
 ### Installing
 
@@ -30,13 +33,13 @@ pip install -r requirements.txt
 
 ## Deployment
 
-* we can add read port option 
+* embedded figure in gui
 
 ## Built With
 
 * [PyQt5](https://www.riverbankcomputing.com/software/pyqt/intro) - Qt application framework used
 * [pyserial](https://pyserial.readthedocs.io/en/latest/pyserial.html) - serial communication framework
-
+* [maplotlib](https://matplotlib.org/3.1.1/contents.html)
 
 
 ## Authors
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import serial
 
 import glob
 
-
+import matplotlib.pyplot as plt
 
 def serial_ports():
     """ Lists serial port names
@@ -42,23 +42,45 @@ def serial_ports():
 
 
 class Main(QtWidgets.QMainWindow, Ui_MainWindow):
-    def __init__(self):
+    def __init__(self, command_length = 7):
+        
         QtWidgets.QMainWindow.__init__(self)
         self.setupUi(self)
-        #click event
+
+        #command length and start
+        self.command_length = command_length
+        self.amp_start = 1
+        self.amp_len = 1
+        self.time_start =  self.amp_start + self.amp_len 
+        self.time_len = 4
+        self.time_end = self.time_start + self.time_len
+
+        #to save reading values
+        self.values = {'amp':[],'time':[]}
+        
+        #send button event
         self.send_btn.clicked.connect(self.send)
-        #click event
+        
+        #send form box event
         self.text_box.returnPressed.connect(self.send)
-        #click event
+        
+        #connect button event
         self.connect_btn.clicked.connect(self.connect)
-        #click event
+        
+        #disconnect button event
         self.disconnect_btn.clicked.connect(self.disconnect)
-        #click event
+        
+        #refresh button event
         self.refersh_btn.clicked.connect(self.refresh)
+        
         # get ports name & set in  combobox
         self.ports_box.addItems(serial_ports())
         self.ser = serial.Serial();
         self.port_name = ''
+
+        #read button click event
+        self.read_btn.clicked.connect(self.read)
+
 
 
 
@@ -71,17 +93,21 @@ class Main(QtWidgets.QMainWindow, Ui_MainWindow):
     def connect(self):
         print("connect clicked")
         self.port_name = self.ports_box.currentText()
+
         if len(self.port_name) != 0:
+
             self.ser = serial.Serial()
             self.ser.baudrate = 9600
             self.ser.port = self.port_name
             self.ser.bytesize = 8
             self.stopbits = 1
+
             if not self.ser.is_open:
                 try:
                     self.ser.open()
                     self.state.setText("Connected")
                     self.state.setStyleSheet("QLabel { color : green; }");
+                
                 except:
                     QMessageBox.about(self, "WARNING", "Can't open port")
 
@@ -93,6 +119,7 @@ class Main(QtWidgets.QMainWindow, Ui_MainWindow):
                 self.ser.close()
                 self.state.setText("Disconnected")
                 self.state.setStyleSheet("QLabel { color : red; }");
+            
             except:
                 QMessageBox.about(self, "WARNING", "Can't close port")
 
@@ -103,6 +130,7 @@ class Main(QtWidgets.QMainWindow, Ui_MainWindow):
             if(self.ser.is_open):
             	print("command : ", command)
             	self.ser.write(command.encode('utf-8'))
+            
             else:
                 QMessageBox.about(self, "WARNING", "Can't write to port (connect on port)")
 
@@ -110,8 +138,55 @@ class Main(QtWidgets.QMainWindow, Ui_MainWindow):
         	print(e)
         	QMessageBox.about(self, "WARNING", "Can't write to port, Exception Occured")
     
+
+    def read(self):
+    	print("read clicked")
+    	if(self.ser.is_open):
+    		while self.ser.inWaiting() > 0:
+    			try:
+	    			s = self.ser.read(self.command_length)
+	    			starter = chr(s[0])
+	    			print('start : ', starter)
+
+
+	    			#Amplitude
+	    			amp = int(s[self.amp_start : self.amp_len + 1].hex(),16)
+	    			print('amp : ', amp)
+	    			self.values['amp'].append(amp)
+
+	    			#Time
+	    			time = int(s[self.time_start : self.time_end].hex(),16)
+	    			print('time : ', time)
+	    			self.values['time'].append(time)
+
+	    			end = chr(s[-1])
+	    			print('end : ',end)
+
+	    			self.read_box.setText('start : ' + str(starter) +' amplitude : ' + str(amp) + ' time : ' + str(time) + ' end ' + str(end))
+
+	    		except:
+	    			print('None')
+
+	    	try:
+	    		if(len(self.values['time']) != 0 or len(self.values['amp']) != 0):
+		    		plt.plot(self.values['time'],self.values['amp'])
+		    		plt.show()
+		    		self.values['amp'] = []
+		    		self.values['time'] = []
+		    
+		    	else:
+		    		QMessageBox.about(self, "INFO", "There are not reading values to plot")
+	    	
+	    	except:
+	    		QMessageBox.about(self, "WARNING", "Can't plotting")
+    	
+    	else:
+    		QMessageBox.about(self, "WARNING", " connect on port firstly")
+
     def closeEvent(self, event):
         self.disconnect()
+
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
